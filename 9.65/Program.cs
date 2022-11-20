4a89112f12a9679a22d2950dd88adcc11e38baa6
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:21:25 +0300

    Add files via upload

---
+++ 9.65/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _9._65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            string txt = Console.ReadLine();
+            float counter = 0;
+            foreach (char symb in txt)
+                if (char.ToLower(symb) == 'а')
+            counter++;
+            Console.WriteLine($"Доля букв \"а\" в предложении: { counter / txt.Length : 0.00 %}");
+        }
+    }
+}
